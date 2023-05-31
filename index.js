270460a763633f12b1e9ec7fb20c43458a49c85d
thien.nguyenminh <thien.nguyenminhgdv@gmail.com>
2023-05-31 14:50:15 +0700

    add nodemon package to automatically reset server when there is any change add npm script to start nodemon testing some api endpoint with get and post request with postman

--- index.js
+++ index.js
@@ -2,9 +2,48 @@ const express = require('express')
 
 const app = express();
 const port = 8000;
+
+
 app.get('/', (req, res) => {
     console.log(req.url);
     res.send('hello');
 })
+
+app.get('/testEndpoint', (req, res) => {
+    res.json({
+        title: 'hello world!',
+        body: 'this is one of json property I created'
+    })
+})
+
+app.get('/user', (req, res) => {
+    res.json({
+        firstName: 'Thien',
+        lastName: 'Nguyen'
+    })
+})
+
+app.get('/users', (req, res) => {
+
+    const jsonArr = []
+    for (let i = 0; i < 5; i++) {
+        var jsonObj = {
+            firstName: `test firstname ${i}`,
+            lastName: `test lastname ${i}`
+        }
+        jsonArr.push(jsonObj)
+    }
+    res.json(jsonArr)
+})
+
+app.post('/testPost', (req, res) => {
+    const postData = req.url;
+    console.log(postData);
+    res.json({
+        message: 'post request recieved!!',
+        reqDataURL: `endpoint: ${postData}`
+    })
+})
+
 
 app.listen(process.env.PORT || port, () => console.log(`App available on http://localhost:${port}`)) 
